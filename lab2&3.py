5883d751e3a8b46a217531142d988181a66d0c5c
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-05-15 22:20:07 +0800

    Almost complete.

--- lab2&3.py
+++ lab2&3.py
@@ -45,7 +45,7 @@ precedence = (
 
 # def p_statement_expr(p):
 #     '''statement : expression'''
-#     d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+#     d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
 #     d['value'] = p[1]['value']
 #     d['code'] = str(p[1]['code'])
 #     p[0] = d
@@ -54,7 +54,7 @@ precedence = (
 
 # def p_statement_cond(p):
 #     '''statement : condition'''
-#     d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+#     d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
 #     d['bool'] = p[1]['bool']
 #     print(p[1]['bool'])
 #     logging.info(str(d) + " when S -> C")
@@ -64,7 +64,7 @@ precedence = (
 # P -> L
 def p_prime_line(p):
     '''prime : line'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = p[1]['code']
     d['bool'] = p[1]['bool']
     p[0] = d
@@ -73,7 +73,7 @@ def p_prime_line(p):
 # P -> LP1
 def p_prime_lineprime(p):
     '''prime : line prime'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = str(p[1]['code']) + ' ' + str(p[2]['code'])
     d['bool'] = p[2]['bool']
     p[0] = d
@@ -82,7 +82,7 @@ def p_prime_lineprime(p):
 # L -> S ;
 def p_line_statement(p):
     '''line : statement ';' '''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = p[1]['code'] + ';'
     d['bool'] = p[1]['bool']
     p[0] = d
@@ -91,7 +91,7 @@ def p_line_statement(p):
 # S -> id = E
 def p_id_statement(p):
     '''statement : IDENTIFIER '=' expression'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = str(p[1]) + "=" + str(p[3]['code'])
     p[0] = d
     logging.info(str(p[0]) + " when S -> id = E ")
@@ -99,7 +99,7 @@ def p_id_statement(p):
 # S -> if C then S1
 def p_statement_if(p):
     '''statement : IF condition THEN statement'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = 'if ' + str(p[2]['code']) + ' then ' + str(p[4]['code'])
     if p[2]['bool'] is True:
         d['bool'] = p[2]['bool']
@@ -113,7 +113,7 @@ def p_statement_if(p):
 # # S -> if C then S1 else S2
 def p_statement_ifelse(p):
     '''statement : IF condition THEN statement ELSE statement'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = 'if ' + str(p[2]['code']) + ' then ' + str(p[4]['code']) + ' else ' +str(p[6]['code'])
     if p[2]['bool'] is True:
         d['bool'] = p[2]['bool']
@@ -127,7 +127,7 @@ def p_statement_ifelse(p):
 # S -> while C do S
 def p_statement_while(p):
     '''statement : WHILE condition DO statement '''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] ='while ' + p[2]['code'] + ' do ' + p[4]['code']
     if p[2]['bool'] is True :
         d['bool'] = p[2]['bool']
@@ -143,7 +143,7 @@ def p_statement_while(p):
 # S -> { P }
 def p_statement_prime(p):
     '''statement : '{' prime '}' '''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['code'] = p[2]['code']
     p[0] = d
     logging.info(str(p[0]) + " when S -> {P}")
@@ -153,7 +153,7 @@ def p_condition_expression(p):
     ''' condition : expression '>' expression
                   | expression '<' expression
                   | expression '=' expression'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     # print(p[2])
 
     if p[2] == '>':
@@ -189,43 +189,47 @@ def p_condition_expression(p):
 # E -> E1 + T
 def p_expression_plus(p):
     '''expression : expression '+' term'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[1]['value'] + p[3]['value']
     d['code'] = str(p[1]['code']) + "+" + str(p[3]['code'])
+    d['place'] = d['code']
     p[0] = d
     logging.info(str(d) + " when E -> E1 + T.")
 
 # E -> E1 - T
 def p_expression_minus(p):
     '''expression : expression '-' term'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[1]['value'] - p[3]['value']
     d['code'] = str(p[1]['code']) + "-" + str(p[3]['code'])
+    d['place'] = d['code']
     p[0] = d
     logging.info(str(d) + " when E -> E1 - T.")
 
 # E -> T
 def p_expression_term(p):
     '''expression : term'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[1]['value']
-    d['code'] = str(p[1]['value'])
+    d['place'] = p[1]['place']
+    d['code'] = str(p[1]['place'])
     p[0] = d
     logging.info(str(d) + " when E -> T.")
 
 # T -> F
 def p_term_factor(p):
     '''term : factor'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[1]['value']
-    d['code'] = p[1]['value']
+    d['place'] = p[1]['place']
+    d['code'] = p[1]['place']
     p[0] = d
     logging.info(str(d) + " when T -> F.")
 
 # T -> T1 * F
 def p_term_multi(p):
     '''term : term '*' factor'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[1]['value'] * p[3]['value']
     d['code'] = str(p[1]['code']) + "*" + str(p[3]['code'])
     p[0] = d
@@ -234,8 +238,11 @@ def p_term_multi(p):
 # T -> T1 / F
 def p_term_div(p):
     '''term : term '/' factor'''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
-    d['value'] = p[1]['value'] / p[3]['value']
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
+    if p[3]['value'] is not 0:
+        d['value'] = p[1]['value'] / p[3]['value']
+    else :
+        d['value'] = 'NaN'
     d['code'] = str(p[1]['code']) + "/" + str(p[3]['code'])
     p[0] = d
     logging.info(str(p[0]) + " when T -> T1 * F.")
@@ -243,17 +250,19 @@ def p_term_div(p):
 # F -> (E)
 def p_factor_expression(p):
     '''factor : '(' expression ')' '''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['value'] = p[2]['value']
-    d['code'] = str(p[2]['value'])
+    d['place'] = p[2]['place']
+    d['code'] = str(p[2]['code'])
     p[0] = d
     logging.info(str(p[0]) + " when F -> (E).")
 
 # F -> id
 def p_factor_id(p):
     '''factor : IDENTIFIER '''
-    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': -1}
+    d = {'code': '', 'bool': None, 'name': '', 'value': None, 'place': None}
     d['name'] = p[1]
+    d['place'] = str(p[1])
     # Todo: Deal with this issue!
     d['value'] = 0
     d['code'] = str(p[1])
@@ -269,9 +278,10 @@ def p_factor_num(p):
               | REAL10
               | REAL8
               | REAL16 '''
-    d = {'code':'','bool':None,'name':'','value':None,'place':-1}
+    d = {'code':'','bool':None,'name':'','value':None,'place':None}
     d['value'] = p[1]
     d['code'] = str(p[1])
+    d['place'] = str(p[1])
     p[0] = d
     logging.info(str(d) + " when F -> NUMBERS.")
 
